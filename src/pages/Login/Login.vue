f4472daf3436cc0f3b1d8091e1ad600c2a07ec2b
liuyi <liuyi@sina.com>
2019-07-08 10:08:31 +0800

    loginType

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -4,13 +4,13 @@
       <div class="login_header">
         <h2 class="login_logo">硅谷外卖</h2>
         <div class="login_header_title">
-          <a href="javascript:;" class="on">短信登录</a>
-          <a href="javascript:;">密码登录</a>
+          <a href="javascript:;" :class="{on: loginType}" @click="loginType=true">短信登录</a>
+          <a href="javascript:;" :class="{on: !loginType}" @click="loginType=false">密码登录</a>
         </div>
       </div>
       <div class="login_content">
         <form>
-          <div class="on">
+          <div :class="{on: loginType}">
             <section class="login_message">
               <input type="tel" maxlength="11" placeholder="手机号">
               <button disabled="disabled" class="get_verification">获取验证码</button>
@@ -23,7 +23,7 @@
               <a href="javascript:;">《用户服务协议》</a>
             </section>
           </div>
-          <div>
+          <div :class="{on: !loginType}">
             <section>
               <section class="login_message">
                 <input type="tel" maxlength="11" placeholder="手机/邮箱/用户名">
@@ -54,6 +54,11 @@
 
 <script type="text/ecmascript-6">
   export default {
+    data () {
+    return {
+      loginType: true
+    }
+     }
   }
 </script>
 
